b11c0d3b6d78243e8f454f34fef6427ac2fcf2bd
Adam Kowalski <adam.kowalski.work@gmail.com>
2020-08-18 18:57:40 -0700

    sauce code font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hurmit Nerd Font:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Sauce Code Pro Nerd Font:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
