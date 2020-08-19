60dd5bfc448a7fa1154cae4b5dc68d979cfc8a6f
Adam Kowalski <adam.kowalski.work@gmail.com>
2020-08-18 19:34:01 -0700

    font size 14

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Sauce Code Pro Nerd Font:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Sauce Code Pro Nerd Font:size=14:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
