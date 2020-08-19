292df4cf44dfcf7df35280073a070b3f99d16c0c
Adam Kowalski <adam.kowalski.work@gmail.com>
2020-08-18 18:00:43 -0700

    gruvbox

--- config.def.h
+++ config.def.h
@@ -96,24 +96,24 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#272727", // BLACK
+	"#cc231c", // RED
+	"#989719", // GREEN
+	"#d79920", // YELLOW
+	"#448488", // BLUE
+	"#b16185", // MAGENTA
+	"#689d69", // CYAN
+	"#a89983", // WHITE
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#928373", // BLACK
+	"#fb4833", // RED
+	"#b8ba25", // GREEN
+	"#fabc2e", // YELLOW
+	"#83b597", // BLUE
+	"#d3859a", // MAGENTA
+	"#8ec07b", // CYAN
+	"#ebdbb2", // WHITE
 
 	[255] = 0,
 
